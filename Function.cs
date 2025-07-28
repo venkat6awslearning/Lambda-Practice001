1e8a6e82712bf8cf27a9ecb87b4a234367774ed8
Ramana <ramantej@gmail.com>
2025-07-28 17:22:17 +0530

    update on Function response

--- Function.cs
+++ Function.cs
@@ -2,22 +2,24 @@ using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
-
 namespace AWSLambdaPractice01;
 
 public class Function
 {
-
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
     /// <param name="input">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public string FunctionHandler(ILambdaContext context)
+    public List<string> FunctionHandler(ILambdaContext context)
     {
-        
+        // Create list and add multiple collections
+        var fruits = new List<string> { "apple", "banana" };
+        fruits.AddRange(new[] { "cherry", "grape", "apple", "pear" });
 
-        return "I am Ramana";
+        // Filter: starts with 'a'
+        var filtered = fruits.Where(f => f.StartsWith("a")).ToList();
+        return filtered;
     }
 }
